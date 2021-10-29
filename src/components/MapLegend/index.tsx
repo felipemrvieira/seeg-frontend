313d3fb7b2f8e628f9cf11fe3aa7d43eeed40715
felipemrvieira <felipemrvieira@gmail.com>
2021-10-29 17:40:01 -0300

    get map info

--- src/components/MapLegend/index.tsx
+++ src/components/MapLegend/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
 	Container,
 	Box,
@@ -9,33 +9,87 @@ import {
 	RangeGradient,
 	BoxGradientNote,
 } from './styles';
+import api from '../../services/api';
 
-const MapLegend: React.FC = () => (
-	<Container>
-		<Box>
-			{/* <header>Emissões não alocadas nos estados</header> */}
-			{/* <div className="box-control__content"> */}
-			<BoxLabel>Emissões não alocadas nos estados</BoxLabel>
-			<BoxValue>4,80%</BoxValue>
-			<BoxLabel>Total de emissões do setor</BoxLabel>
-			<BoxValue>2.175.630.937</BoxValue>
-			<BoxNote>Em toneladas</BoxNote>
-			{/* </div> */}
-		</Box>
-		<Box>
-			<div className="map-legend-gradient">
-				<Range>
-					<span>0</span>
-					<span>400m</span>
-				</Range>
-				<RangeGradient />
-				<BoxGradientNote>
-					As cores do mapa representam faixas de quantidade de emissões em
-					milhões de toneladas do GEE
-				</BoxGradientNote>
-			</div>
-		</Box>
-	</Container>
-);
+interface ActiveSector {
+	slug: string;
+}
+interface MapInfo {
+	activeSector: ActiveSector;
+	activeYear: number;
+	activeGas: number;
+}
 
+const MapLegend: React.FC<MapInfo> = ({
+	activeSector,
+	activeYear,
+	activeGas,
+}) => {
+	const [mapLegend, setMapLegend] = useState({});
+
+	async function getInfos() {
+		console.log('getInfos');
+
+		try {
+			const params = {
+				sector: activeSector.slug,
+				year: activeYear,
+				gas: activeGas,
+				cities: false,
+			};
+			const response = await api.get('/map/emissions_info', { params });
+			setMapLegend(response.data);
+			console.log(response.data);
+		} catch (err) {
+			// console.tron.log(err);
+		}
+	}
+
+	useEffect(() => {
+		getInfos();
+	}, [activeSector, activeYear, activeGas]);
+
+	// function getInfos(props) {
+	// const params = {
+	// sector: props.activeSector.slug,
+	// year: props.activeYear,
+	// gas: props.activeGas,
+	// cities: IS_CITIES,
+	// };
+
+	// Api.getEmissionsInfo(params).done((data) => {
+	//   this.setState({
+	//     info: data,
+	//   });
+	// });
+	// }
+
+	return (
+		<Container>
+			<Box>
+				{/* <header>Emissões não alocadas nos estados</header> */}
+				{/* <div className="box-control__content"> */}
+				<BoxLabel>Emissões não alocadas nos estados</BoxLabel>
+				<BoxValue>4,80%</BoxValue>
+				<BoxLabel>Total de emissões do setor</BoxLabel>
+				<BoxValue>2.175.630.937</BoxValue>
+				<BoxNote>Em toneladas</BoxNote>
+				{/* </div> */}
+			</Box>
+			<Box>
+				<div className="map-legend-gradient">
+					<Range>
+						<span>0</span>
+						<span>400m</span>
+					</Range>
+					<RangeGradient />
+					<BoxGradientNote>
+						As cores do mapa representam faixas de quantidade de emissões em
+						milhões de toneladas do GEE
+					</BoxGradientNote>
+				</div>
+			</Box>
+		</Container>
+	);
+};
 export default MapLegend;
--- src/pages/State/index.tsx
+++ src/pages/State/index.tsx
@@ -9,8 +9,51 @@ import MapYearFilter from '../../components/MapYearFilter';
 import 'leaflet/dist/leaflet.css';
 import api from '../../services/api';
 
+interface EmissionType {
+	name: string;
+	slug: string;
+}
+interface Gas {
+	id: number;
+	name: string;
+	slug: string;
+}
+interface Sector {
+	id: number;
+	name: string;
+	slug: string;
+}
+interface State {
+	id: number;
+	name: string;
+	slug: string;
+}
+interface MapInfoInterface {
+	activeLevel: number;
+	allSectorsColor: string;
+	defaultEmissionType: string;
+	defaultGas: number;
+	defaultSector: number;
+	defaultYear: number;
+	sectors: Sector[];
+	emissionsTypes: EmissionType[];
+	gases: Gas[];
+	states: State[];
+}
+
 const State: React.FC = () => {
-	const [mapInfo, setMapInfo] = useState({});
+	const [mapInfo, setMapInfo] = useState<MapInfoInterface>({
+		activeLevel: 0,
+		allSectorsColor: '#00a8ff',
+		defaultEmissionType: 'CO2',
+		defaultGas: 0,
+		defaultSector: 0,
+		defaultYear: 2020,
+		sectors: [],
+		emissionsTypes: [],
+		gases: [],
+		states: [],
+	});
 
 	async function loadStateMapInfo() {
 		try {
@@ -20,6 +63,10 @@ const State: React.FC = () => {
 		} catch (err) {
 			// console.tron.log(err);
 		}
+	}
+
+	function activeSector() {
+		return mapInfo && mapInfo.sectors.filter((obj) => obj.id === 0)[0];
 	}
 
 	useEffect(() => {
@@ -43,7 +90,11 @@ const State: React.FC = () => {
 					/>
 				</MapContainer>
 				<MapFilters />
-				<MapLegend />
+				<MapLegend
+					activeSector={activeSector()}
+					activeGas={mapInfo.defaultGas}
+					activeYear={mapInfo.defaultYear}
+				/>
 			</Container>
 			<MapYearFilter />
 			<Footer />
