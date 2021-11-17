ec58da2085d42e61c84fa9beebca85e30a9b6d43
felipemrvieira <felipemrvieira@gmail.com>
2021-11-17 11:52:32 -0300

    create header boxes

--- src/components/CardHeader/index.tsx
+++ src/components/CardHeader/index.tsx
@@ -2,5 +2,23 @@ import React from 'react';
 
 import { Container } from './styles';
 
-const CardHeader: React.FC = () => <Container>CardHeader</Container>;
+const CardHeader: React.FC = () => {
+	console.log('ads');
+
+	return (
+		<Container>
+			<header>
+				<img
+					src="https://s3-sa-east-1.amazonaws.com/seeg.ecostage.com.br/flags/15.jpg"
+					alt="flag"
+				/>
+				<h1>Pará - 2020</h1>
+			</header>
+			<div className="boxes">
+				<div className="mainBox">princ</div>
+				<div className="subBoxes">sub</div>
+			</div>
+		</Container>
+	);
+};
 export default CardHeader;
--- src/components/CardHeader/styles.js
+++ src/components/CardHeader/styles.js
@@ -3,10 +3,51 @@ import styled from 'styled-components';
 export const Container = styled.div`
 	display: flex;
 	flex: 1;
-	flex-direction: ol;
+	flex-direction: column;
 	width: 100%;
 	background: #ecf0f0;
-	min-height: 75vh;
+	/* min-height: 75vh; */
 	justify-content: space-between;
-	/* padding: 10px; */
+	padding: 32px;
+	header {
+		width: 100%;
+		height: inherit;
+		display: flex;
+		align-items: center;
+		margin-bottom: 30px;
+
+		/* justify-content: center; */
+		img {
+			max-width: 100px;
+			height: inherit;
+			margin-right: 15px;
+			display: inline-block;
+		}
+		h1 {
+			display: inline-block;
+
+			font-size: 2.5em;
+			font-weight: 300;
+			color: #1e2229;
+			font-family: 'Lato', sans-serif;
+			line-height: 1.25;
+			margin: 0;
+			text-rendering: optimizeLegibility;
+		}
+	}
+
+	.boxes {
+		display: flex;
+		flex-direction: row;
+		width: 100%;
+		justify-content: space-between;
+		.mainBox {
+			background: red;
+			flex: 1;
+		}
+		.subBoxes {
+			background: blue;
+			flex: 2;
+		}
+	}
 `;
