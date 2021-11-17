b493cb9eaabf5f0b0b1db65617d0b09762fcd07f
felipemrvieira <felipemrvieira@gmail.com>
2021-11-16 21:28:23 -0300

    apply loader

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,16 +1,21 @@
 import React, { useState, useEffect, useMemo } from 'react';
+import axios from 'axios';
 import {
 	MapContainer,
 	TileLayer,
 	ZoomControl,
 	WMSTileLayer,
-	Marker,
 	useMapEvents,
+	useMap,
 } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import NumberFormat from 'react-number-format';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
 import { MapInfo, PositionState } from './interfaces';
 import { StyledPop as Popup } from './styles';
 import Header from '../Header';
+import api from '../../services/api';
 
 const Map: React.FC<MapInfo> = ({
 	activeSector,
@@ -23,14 +28,129 @@ const Map: React.FC<MapInfo> = ({
 		lat: 0,
 		lng: 0,
 	});
+	const layerName = isCity
+		? 'seeg-latest-version:geoserver_cities'
+		: 'seeg8-br-plataform-staging:seeg8-staging-geoserver-states';
 
 	const [showPopUp, setShowPopUp] = useState(false);
+	const [open, setOpen] = useState(false);
+	const [popUpInfo, setPopUpInfo] = useState({
+		area: 0,
+		gas_id: 0,
+		ibge_cod: 0,
+		id: 0,
+		max_emission_of_same_type_sector_gas_and_year: 0.0,
+		name: '',
+		rank: 0,
+		sector_id: 0,
+		slug: '',
+		total_emission: 0.0,
+		total_population: 0,
+		urban_population: 0,
+		year: 2020,
+	});
+	const [totalAllocatedEmission, setTotalAllocatedEmission] = useState('0,0');
+	const [zoom, setZoom] = useState(4);
 
-	function MapEventsChildComponent() {
-		const map = useMapEvents({
-			popupclose: () => console.log('popupclose'),
-			click(event) {
+	const handleClose = () => {
+		setOpen(false);
+	};
+	async function getPopUpChartInfo(slug: string) {
+		try {
+			const response = await api.get(
+				`/territories/${slug}/popup_chart?sector=all&year=${activeYear}&gas=${activeGas}`
+			);
+
+			setTotalAllocatedEmission(response.data.total_allocated_emission);
+			console.log(response.data);
+		} catch (err) {
+			// console.tron.log(err);
+		}
+	}
+
+	async function getPopUpInfo(mapSize: any, bbox: any, relativePoint: any) {
+		try {
+			const urlPath = 'https://geoserver.ecostage.com.br/geoserver/wfs';
+
+			const params = [
+				['request', 'GetFeatureInfo'],
+				['service', 'WMS'],
+				['version', '1.3.0'],
+				['layers', layerName],
+				['query_layers', layerName],
+				['styles', ''],
+				['crs', 'EPSG:4674'],
+				[
+					'bbox',
+					`${bbox.southWest.lat},${bbox.southWest.lng},${bbox.northEast.lat},${bbox.northEast.lng}`,
+				],
+				['width', mapSize.x],
+				['height', mapSize.y],
+				['info_format', 'application/json'],
+				['feature_count', 1],
+				['i', Math.round(relativePoint.x)],
+				['j', Math.round(relativePoint.y)],
+				['exceptions', 'application/json'],
+				[
+					'cql_filter',
+					`year=${activeYear} and sector_id=${activeSector.id} and gas_id=${activeGas}`,
+				],
+				[
+					'propertyName',
+					'id,slug,name,ibge_cod,year,gas_id,sector_id,rank,total_population,urban_population,area,total_emission,max_emission_of_same_type_sector_gas_and_year',
+				], // essas são todas as props disponíveis para serem consultadas. "id" é um territory_id
+			];
+
+			try {
+				const response = await axios.get(
+					`${urlPath}?${params.map((e) => e.join('=')).join('&')}`
+				);
+				console.log(response.data.features[0].properties);
+				setPopUpInfo(response.data.features[0].properties);
+				getPopUpChartInfo(response.data.features[0].properties.slug);
+			} catch (error) {
+				// add error handling
+				setOpen(false);
+				console.log(error);
+			}
+		} catch (err) {
+			// console.tron.log(err);
+		}
+	}
+
+	function MapChildComponent() {
+		useEffect(() => {
+			console.log('Render Map Child');
+		}, []);
+
+		const map = useMap();
+		const mapEvents = useMapEvents({
+			popupopen: (e) => {
+				// getPopUpChartInfo();
+				setOpen(false);
+				console.log('open');
+			},
+			popupclose: (e) => {
+				setShowPopUp(false);
+				setTotalAllocatedEmission('0,0');
+			},
+			click: (event) => {
+				setOpen(true);
 				const { lat, lng } = event.latlng;
+				const mapSize = {
+					x: map.getSize().x,
+					y: map.getSize().y,
+				};
+
+				const bbox = {
+					southWest: map.getBounds().getSouthWest(),
+					northEast: map.getBounds().getNorthEast(),
+				};
+
+				const relativePoint = map.latLngToContainerPoint(event.latlng);
+
+				getPopUpInfo(mapSize, bbox, relativePoint);
+
 				setShowPopUp(true);
 				setPopUpPosition({
 					lat,
@@ -85,13 +205,20 @@ const Map: React.FC<MapInfo> = ({
 
 	return (
 		<>
+			<Backdrop
+				sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+				open={open}
+				onClick={handleClose}
+			>
+				<CircularProgress color="inherit" />
+			</Backdrop>
 			<MapContainer
 				zoomControl={false}
 				center={[-18.5, -54]}
-				zoom={4}
+				zoom={zoom}
 				scrollWheelZoom={false}
 			>
-				<MapEventsChildComponent />
+				<MapChildComponent />
 				<ZoomControl position="topright" />
 				<TileLayer
 					attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
@@ -115,35 +242,57 @@ const Map: React.FC<MapInfo> = ({
 						/> */}
 					</>
 				)}
-				{showPopUp && (
+				{showPopUp && totalAllocatedEmission !== '0,0' && (
 					<Popup position={[popUpPosition.lat, popUpPosition.lng]}>
 						{/* Latitude {popUpPosition.lat}.
 						<br />
 						Longitude {popUpPosition.lng}. */}
 						<div className="popUpHeader">
 							<img
-								src="https://s3-sa-east-1.amazonaws.com/seeg.ecostage.com.br/flags/15.jpg"
+								src={`https://s3-sa-east-1.amazonaws.com/seeg.ecostage.com.br/flags/${popUpInfo.ibge_cod}.jpg`}
 								alt="flag"
 							/>
-							<span>Pará</span>
+							<span>{popUpInfo.name}</span>
 						</div>
 						<div className="popUpTerritoryInfo">
 							<div className="info">
 								<div className="label">Total Pop.</div>
-								<div className="value">8,690,745</div>
+								<div className="value">
+									<NumberFormat
+										thousandSeparator
+										prefix=""
+										displayType="text"
+										value={popUpInfo.total_population}
+									/>
+								</div>
 							</div>
 							<div className="info">
 								<div className="label">Area</div>
-								<div className="value">1,245,871</div>
+								<div className="value">
+									<NumberFormat
+										thousandSeparator
+										prefix=""
+										displayType="text"
+										value={popUpInfo.area}
+									/>
+								</div>
 							</div>
 							<div className="info">
 								<div className="label">Ranking</div>
-								<div className="value">1</div>
+								<div className="value">{popUpInfo.rank}</div>
 							</div>
 						</div>
 						<div className="popUpEmissionInfo">
 							<div className="label">Emissions allocated in the state</div>
-							<div className="value">416.987.095 ton</div>
+							<div className="value">
+								<NumberFormat
+									thousandSeparator
+									prefix=""
+									displayType="text"
+									value={totalAllocatedEmission?.replaceAll('.', '')}
+								/>{' '}
+								ton
+							</div>
 						</div>
 						<div className="popUpGraphic">asd</div>
 						<div className="popUpButton">
