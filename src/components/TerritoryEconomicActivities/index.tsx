03113d950d7b6567ccbb890a126ad8a437e4f43d
felipemrvieira <felipemrvieira@gmail.com>
2021-11-23 10:20:13 -0300

    Prepare territoryEconomicActivities area

--- src/components/TerritoryEconomicActivities/index.tsx
+++ src/components/TerritoryEconomicActivities/index.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+
 import { Container } from './styles';
 
 const TerritoryEconomicActivities: React.FC = () => (
-	<Container>TerritoryEconomicActivities</Container>
+	<Container>
+		<div className="header">
+			<h2>Atividades Econômicas Estaduais</h2>
+			<p>
+				O diagrama abaixo mostra as emissões de GEE do estado por setor,
+				subsetor e atividade econômica (da esquerda para a direita)
+			</p>
+		</div>
+		<div className="chartWrapper">
+			<div className="chart">
+				<p>D3 Sankey chart</p>
+			</div>
+		</div>
+	</Container>
 );
 export default TerritoryEconomicActivities;
