5ca388ae6d1b2e5ba98ba6081043d06878281278
felipemrvieira <felipemrvieira@gmail.com>
2021-11-29 18:29:10 -0300

    fix accorion error

--- src/components/SectorCharts/index.tsx
+++ src/components/SectorCharts/index.tsx
@@ -94,53 +94,55 @@ const SectorCharts: React.FC = () => {
 						<i className="icon energy" />
 						<h3 className="title">Energia</h3>
 					</div>
-					<table className="infoWrapper">
-						<tbody>
-							<Accordion>
-								<AccordionSummary
-									expandIcon={<ExpandMoreIcon />}
-									aria-controls="panel1a-content"
-									id="panel1a-header"
-								>
-									<p>Percentuais</p>
-								</AccordionSummary>
-								<AccordionDetails>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">Transporte</td>
-									</tr>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">Energia</td>
-									</tr>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">
-											Geração de Eletricidade (Serviço Público)
-										</td>
-									</tr>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">
-											Geração de Eletricidade (Serviço Público)
-										</td>
-									</tr>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">
-											Geração de Eletricidade (Serviço Público)
-										</td>
-									</tr>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">
-											Geração de Eletricidade (Serviço Público)
-										</td>
-									</tr>
-								</AccordionDetails>
-							</Accordion>
-						</tbody>
-					</table>
+					<div className="infoWrapper">
+						<Accordion>
+							<AccordionSummary
+								expandIcon={<ExpandMoreIcon />}
+								aria-controls="panel1a-content"
+								id="panel1a-header"
+							>
+								<p>Percentuais</p>
+							</AccordionSummary>
+							<AccordionDetails>
+								<table>
+									<tbody>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">Transporte</td>
+										</tr>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">Energia</td>
+										</tr>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">
+												Geração de Eletricidade (Serviço Público)
+											</td>
+										</tr>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">
+												Geração de Eletricidade (Serviço Público)
+											</td>
+										</tr>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">
+												Geração de Eletricidade (Serviço Público)
+											</td>
+										</tr>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">
+												Geração de Eletricidade (Serviço Público)
+											</td>
+										</tr>
+									</tbody>
+								</table>
+							</AccordionDetails>
+						</Accordion>
+					</div>
 					<HighchartsReact highcharts={Highcharts} options={options} />
 				</div>
 				<div className="sector">
@@ -148,53 +150,55 @@ const SectorCharts: React.FC = () => {
 						<i className="icon tree" />
 						<h3 className="title">Uso da terra</h3>
 					</div>
-					<table className="infoWrapper">
-						<tbody>
-							<Accordion>
-								<AccordionSummary
-									expandIcon={<ExpandMoreIcon />}
-									aria-controls="panel1a-content"
-									id="panel1a-header"
-								>
-									<p>Percentuais</p>
-								</AccordionSummary>
-								<AccordionDetails>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">Transporte</td>
-									</tr>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">Energia</td>
-									</tr>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">
-											Geração de Eletricidade (Serviço Público)
-										</td>
-									</tr>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">
-											Geração de Eletricidade (Serviço Público)
-										</td>
-									</tr>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">
-											Geração de Eletricidade (Serviço Público)
-										</td>
-									</tr>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">
-											Geração de Eletricidade (Serviço Público)
-										</td>
-									</tr>
-								</AccordionDetails>
-							</Accordion>
-						</tbody>
-					</table>
+					<div className="infoWrapper">
+						<Accordion>
+							<AccordionSummary
+								expandIcon={<ExpandMoreIcon />}
+								aria-controls="panel1a-content"
+								id="panel1a-header"
+							>
+								<p>Percentuais</p>
+							</AccordionSummary>
+							<AccordionDetails>
+								<table>
+									<tbody>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">Transporte</td>
+										</tr>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">Energia</td>
+										</tr>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">
+												Geração de Eletricidade (Serviço Público)
+											</td>
+										</tr>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">
+												Geração de Eletricidade (Serviço Público)
+											</td>
+										</tr>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">
+												Geração de Eletricidade (Serviço Público)
+											</td>
+										</tr>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">
+												Geração de Eletricidade (Serviço Público)
+											</td>
+										</tr>
+									</tbody>
+								</table>
+							</AccordionDetails>
+						</Accordion>
+					</div>
 					<HighchartsReact highcharts={Highcharts} options={options} />
 				</div>
 				<div className="sector">
@@ -202,53 +206,55 @@ const SectorCharts: React.FC = () => {
 						<i className="icon cow" />
 						<h3 className="title">Agropecuária</h3>
 					</div>
-					<table className="infoWrapper">
-						<tbody>
-							<Accordion>
-								<AccordionSummary
-									expandIcon={<ExpandMoreIcon />}
-									aria-controls="panel1a-content"
-									id="panel1a-header"
-								>
-									<p>Percentuais</p>
-								</AccordionSummary>
-								<AccordionDetails>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">Transporte</td>
-									</tr>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">Energia</td>
-									</tr>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">
-											Geração de Eletricidade (Serviço Público)
-										</td>
-									</tr>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">
-											Geração de Eletricidade (Serviço Público)
-										</td>
-									</tr>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">
-											Geração de Eletricidade (Serviço Público)
-										</td>
-									</tr>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">
-											Geração de Eletricidade (Serviço Público)
-										</td>
-									</tr>
-								</AccordionDetails>
-							</Accordion>
-						</tbody>
-					</table>
+					<div className="infoWrapper">
+						<Accordion>
+							<AccordionSummary
+								expandIcon={<ExpandMoreIcon />}
+								aria-controls="panel1a-content"
+								id="panel1a-header"
+							>
+								<p>Percentuais</p>
+							</AccordionSummary>
+							<AccordionDetails>
+								<table>
+									<tbody>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">Transporte</td>
+										</tr>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">Energia</td>
+										</tr>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">
+												Geração de Eletricidade (Serviço Público)
+											</td>
+										</tr>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">
+												Geração de Eletricidade (Serviço Público)
+											</td>
+										</tr>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">
+												Geração de Eletricidade (Serviço Público)
+											</td>
+										</tr>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">
+												Geração de Eletricidade (Serviço Público)
+											</td>
+										</tr>
+									</tbody>
+								</table>
+							</AccordionDetails>
+						</Accordion>
+					</div>
 					<HighchartsReact highcharts={Highcharts} options={options} />
 				</div>
 				<div className="sector">
@@ -256,53 +262,55 @@ const SectorCharts: React.FC = () => {
 						<i className="icon trash" />
 						<h3 className="title">Resíduos</h3>
 					</div>
-					<table className="infoWrapper">
-						<tbody>
-							<Accordion>
-								<AccordionSummary
-									expandIcon={<ExpandMoreIcon />}
-									aria-controls="panel1a-content"
-									id="panel1a-header"
-								>
-									<p>Percentuais</p>
-								</AccordionSummary>
-								<AccordionDetails>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">Transporte</td>
-									</tr>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">Energia</td>
-									</tr>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">
-											Geração de Eletricidade (Serviço Público)
-										</td>
-									</tr>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">
-											Geração de Eletricidade (Serviço Público)
-										</td>
-									</tr>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">
-											Geração de Eletricidade (Serviço Público)
-										</td>
-									</tr>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">
-											Geração de Eletricidade (Serviço Público)
-										</td>
-									</tr>
-								</AccordionDetails>
-							</Accordion>
-						</tbody>
-					</table>
+					<div className="infoWrapper">
+						<Accordion>
+							<AccordionSummary
+								expandIcon={<ExpandMoreIcon />}
+								aria-controls="panel1a-content"
+								id="panel1a-header"
+							>
+								<p>Percentuais</p>
+							</AccordionSummary>
+							<AccordionDetails>
+								<table>
+									<tbody>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">Transporte</td>
+										</tr>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">Energia</td>
+										</tr>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">
+												Geração de Eletricidade (Serviço Público)
+											</td>
+										</tr>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">
+												Geração de Eletricidade (Serviço Público)
+											</td>
+										</tr>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">
+												Geração de Eletricidade (Serviço Público)
+											</td>
+										</tr>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">
+												Geração de Eletricidade (Serviço Público)
+											</td>
+										</tr>
+									</tbody>
+								</table>
+							</AccordionDetails>
+						</Accordion>
+					</div>
 					<HighchartsReact highcharts={Highcharts} options={options} />
 				</div>
 				<div className="sector">
@@ -310,53 +318,55 @@ const SectorCharts: React.FC = () => {
 						<i className="icon factory" />
 						<h3 className="title">Indústria</h3>
 					</div>
-					<table className="infoWrapper">
-						<tbody>
-							<Accordion>
-								<AccordionSummary
-									expandIcon={<ExpandMoreIcon />}
-									aria-controls="panel1a-content"
-									id="panel1a-header"
-								>
-									<p>Percentuais</p>
-								</AccordionSummary>
-								<AccordionDetails>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">Transporte</td>
-									</tr>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">Energia</td>
-									</tr>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">
-											Geração de Eletricidade (Serviço Público)
-										</td>
-									</tr>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">
-											Geração de Eletricidade (Serviço Público)
-										</td>
-									</tr>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">
-											Geração de Eletricidade (Serviço Público)
-										</td>
-									</tr>
-									<tr className="info">
-										<td>99%</td>
-										<td className="label">
-											Geração de Eletricidade (Serviço Público)
-										</td>
-									</tr>
-								</AccordionDetails>
-							</Accordion>
-						</tbody>
-					</table>
+					<div className="infoWrapper">
+						<Accordion>
+							<AccordionSummary
+								expandIcon={<ExpandMoreIcon />}
+								aria-controls="panel1a-content"
+								id="panel1a-header"
+							>
+								<p>Percentuais</p>
+							</AccordionSummary>
+							<AccordionDetails>
+								<table>
+									<tbody>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">Transporte</td>
+										</tr>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">Energia</td>
+										</tr>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">
+												Geração de Eletricidade (Serviço Público)
+											</td>
+										</tr>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">
+												Geração de Eletricidade (Serviço Público)
+											</td>
+										</tr>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">
+												Geração de Eletricidade (Serviço Público)
+											</td>
+										</tr>
+										<tr className="info">
+											<td>99%</td>
+											<td className="label">
+												Geração de Eletricidade (Serviço Público)
+											</td>
+										</tr>
+									</tbody>
+								</table>
+							</AccordionDetails>
+						</Accordion>
+					</div>
 					<HighchartsReact highcharts={Highcharts} options={options} />
 				</div>
 			</div>
